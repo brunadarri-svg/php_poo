95844a3c81e11a15f3e12545f16e4d4230dc6ba5
bruna darri <bruna.darri@aluno.senai.br>
2026-03-11 14:03:16 -0300

    feito novamente pois tinha um erro

--- exemplo05.php
+++ exemplo05.php
@@ -6,7 +6,7 @@ class funcionario
     public $salario;
     public $previdencia;
     public $descontos;
-    public $tipoFuncionarios;
+    public $tipoFuncionario;
 
     function __construct($nome, $salario, $previdencia )
     {
@@ -14,27 +14,29 @@ class funcionario
          $this->salario = $salario;
          $this->previdencia = $previdencia;
          $this->descontos= $this->calcularDescontos();
-         $this->tipoFuncionario = $this->categotizarFuncionario();
+         $this->tipoFuncionario = $this->categorizarFuncionario();
     }
 
     function calcularDescontos()
     {
        return number_format($this->salario * 0.257 + $this->previdencia, 2, ',','.');
     }
-    function categorizarFuncionario():string
+
+    function categorizarFuncionario(): string 
     {
         $salario = $this->salario;
 
         if($salario >= 1000 && $salario < 2000){
-            $tipoFuncionario = "Júnior";
-        } else if ($salario >= 2000 && $salario <3000){
-            $tipoFuncionario ="pleno";
-        } else if ($salario >= 3000) {
+            $tipoFuncionario = "Junior";
+        }else if($salario >= 2000 && $salario < 3000){
+            $tipoFuncionario = "Pleno";
+        }else if($salario >= 3000 ){
             $tipoFuncionario = "Senior";
-        } else {
-            $tipoFuncionario = "estagiário";
+        }else{
+            $tipoFuncionario = "Estagiario";
         }
-         return $tipoFuncionario;
+
+        return $tipoFuncionario;
     }
 }
 
@@ -42,15 +44,12 @@ $joao = new Funcionario('Joao Filho', 1000,100);
 $maria = new Funcionario('Maria Rute', 2000,200);
 $jose = new Funcionario('José Salgado', 3000,300);
 
-echo "Funcionari@ $joao->nome é $joao->tipoFuncionario e o Valor do desconto
- é de R$ $joao->descontos.
+echo "Funcionari@ $joao->nome é   $joao->tipoFuncionario e o valor do desconto é
+de R$ $joao->descontos <br>";
 
+echo "Funcionari@ $maria->nome  é $maria->tipoFuncionario e o valor do desconto é
+de R$ $maria->descontos <br>";
 
-echo "Funcionari@ $maria->nome é $maria->tipoFuncionario e o Valor do desconto
- é de R$ $maria->descontos. <br>";
-
-
-echo "Funcionari@ $jose->nome é $jose->tipoFuncionario e o Valor do desconto
- é de R$ $jose->descontos. <br>";
-
+echo "Funcionari@ $jose->nome é   $jose->tipoFuncionario e o valor do desconto é
+de R$ $jose->descontos <br>";
 
